fb12fb4fb1025c147dc91558dc58ff90bbceca9e
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-09-24 18:07:15 +0300

    send URL and Title of the page

--- Extension/Action.js
+++ Extension/Action.js
@@ -3,6 +3,11 @@ var Action = function() {};
 Action.prototype = {
 
 run: function(parameters) {
+    
+    //to send two pieces of data to our extension
+    
+    //"tell iOS the JavaScript has finished preprocessing, and give this data dictionary to the extension." The data that is being sent has the keys "URL" and "title", with the values being the page URL and page title.
+    parameters.completionFunction({"URL": document.URL , "title": document.title});
 
 },
 
